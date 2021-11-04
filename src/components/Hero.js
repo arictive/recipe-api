e3c92773babf44c078fae519e0bff756782a9dd3
arictive <nibrosmaverick3@gmail.com>
2021-11-04 22:00:55 +0700

    Styling hero section & input form

---
+++ src/components/Hero.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Hero = () => (
+  <div className="py-14">
+    <h1 className="font-extrabold text-4xl">What recipe do you</h1>
+    <h1 className="pt-2 font-extrabold text-4xl text-indigo-600">want to know?</h1>
+  </div>
+)
+
+export default Hero
